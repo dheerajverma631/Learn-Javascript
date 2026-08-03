993954f7ab6ac58785e9bf3764e1cbe572b4c978
Dheeraj Verma <dheerajvrma63@gmail.com>
2026-08-03 09:22:33 +0000

    DataTypes Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";  //    Treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3
+    +
+    3);  // code readability should be high , not to write this way ,
+    // not a good practice
+
+console.log("Dheeraj");
+
+let name = "Dheeraj"
+let age = 18
+let isloggedin = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof age);
+
+console.log(typeof null);   // object
+
+console.log(typeof undefined)  //udefined
+console.log(typeof isloggedin);
+console.log(typeof state)
+console.log(typeof name)
+
+
+
