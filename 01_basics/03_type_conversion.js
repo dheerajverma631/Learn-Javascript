6a1af99328004608563c1130df1d9b4d3e053a64
Dheeraj Verma <dheerajvrma63@gmail.com>
2026-08-04 06:56:21 +0000

    type conversion done

---
+++ 01_basics/03_type_conversion.js
@@ -0,0 +1,42 @@
+let score = "Dheeraj"
+
+// console.log(typeof score);   ~ can be written in both ways
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "Dheeraj" => NaN
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isloggedIn = "Dheeraj"
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// // 1 => true; 0 => false
+// // "" => false  ~if there is value in the qoutation then it's 'true' otherwise 'false'
+// // "DHeeraj" => true
+
+let someNumber = 33
+// console.log(typeof someNumber);
+
+
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+
+
+
+
+
+
